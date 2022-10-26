6aed8c8aea1b502c8bd67d81f72df42c006af156
Yaphi Berhanu <yaphi1@gmail.com>
2022-10-26 16:43:39 -0400

    Improve lighting

--- src/scripts/setLights.js
+++ src/scripts/setLights.js
@@ -4,14 +4,15 @@ export default function (scene) {
   const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
   scene.add(ambientLight);
 
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2)
-  directionalLight.castShadow = true
-  directionalLight.shadow.mapSize.set(1024, 1024)
-  directionalLight.shadow.camera.far = 15
-  directionalLight.shadow.camera.left = - 7
-  directionalLight.shadow.camera.top = 7
-  directionalLight.shadow.camera.right = 7
-  directionalLight.shadow.camera.bottom = - 7
-  directionalLight.position.set(5, 5, 5)
-  scene.add(directionalLight)
+  const directionalLight = new THREE.DirectionalLight(0xffffff, 0.6);
+  directionalLight.castShadow = true;
+  directionalLight.shadow.mapSize.set(1024, 1024);
+  directionalLight.shadow.camera.far = 15;
+  directionalLight.shadow.camera.left = - 7;
+  directionalLight.shadow.camera.top = 7;
+  directionalLight.shadow.camera.right = 7;
+  directionalLight.shadow.camera.bottom = - 7;
+
+  directionalLight.position.set(5, 5, 5);
+  scene.add(directionalLight);
 };
