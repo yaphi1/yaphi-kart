8291f23112ee4e31e577a625685badcee5e72a91
Yaphi Berhanu <yaphi1@gmail.com>
2022-10-26 19:56:32 -0400

    Improve audio looping

--- src/scripts/audio.js
+++ src/scripts/audio.js
@@ -11,8 +11,8 @@ audio.sfx.engineSound.minVolume = 0.1;
 audio.sfx.engineSound.maxVolume = 0.4;
 audio.sfx.engineSound.volume = audio.sfx.engineSound.minVolume;
 audio.sfx.engineSound.addEventListener('timeupdate', function() {
-	const buffer = 0.64;
-	if (this.currentTime > this.duration - buffer) {
+	const offset = 0.64;
+	if (this.currentTime > this.duration - offset) {
 		this.currentTime = 0.1;
 		this.play();
 	}
@@ -21,7 +21,17 @@ audio.sfx.engineSound.addEventListener('timeupdate', function() {
 audio.tracks.warioStadium = new Audio('https://ia903401.us.archive.org/13/items/mario-kart-64-original-soundtrack/1.02%20Raceway%20%26%20Wario%20Stadium.flac');
 audio.tracks.warioStadium.setAttribute('loop', true);
 
-audio.bgMusic = audio.tracks.warioStadium;
+audio.tracks.toadsTurnpike = new Audio('https://ia903401.us.archive.org/13/items/mario-kart-64-original-soundtrack/1.06%20Toad%27s%20Turnpike.mp3');
+audio.tracks.toadsTurnpike.setAttribute('loop', true);
+audio.tracks.toadsTurnpike.addEventListener('timeupdate', function() {
+	const offset = 10.4;
+	if (this.currentTime > this.duration - offset) {
+		this.currentTime = 14.05;
+		this.play();
+	}
+});
+
+audio.bgMusic = audio.tracks.toadsTurnpike;
 
 audio.applyAcceleration = function ({ isAccelerating }) {
   const { minVolume, maxVolume } = audio.sfx.engineSound;
